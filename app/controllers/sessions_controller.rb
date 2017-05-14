323388e80568396de982fbdd5f2a2d03ababc86f
Billy Chang <billyheungwing@yahoo.ca>
2017-05-14 18:31:16 +0000

    fixed login fail path and flashes, and redirect to root when click on cart if cart empty with flash notification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now.alert = "Email or password is invalid"
-      redirect_to '/login'
+      flash.alert = "Email or password is invalid"
+      redirect_to new_session_path
     end
   end
 
